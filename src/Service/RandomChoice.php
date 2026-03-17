31b0f2e9c82aa192aa3d7fc9ff64f84ff31bd2a3
lukehayes <lukepurplehayes@gmail.com>
2026-03-17 08:50:00 +0000

    feat: add RandomChoice::chooseFromOptions()

--- src/Service/RandomChoice.php
+++ src/Service/RandomChoice.php
@@ -16,6 +16,29 @@ class RandomChoice
         $this->options = $options;
     }
 
+	/**
+	* Choose from a supplied list of options.
+	*
+	* @param array $options    The options to chose from
+	*
+	* @throws Exception
+	*
+	* @return mixed    A selected value from the list if it exists
+	*/
+    public function chooseFromOptions(array $options)
+    {
+		$this->setOptions($options);
+
+        if(!empty($this->options))
+        {
+            $randomIndex = random_int(0, count($this->options) - 1);
+            return $this->options[$randomIndex];
+        }else
+        {
+            throw new \Exception(__CLASS__ . " has no options to choose from. Try adding some with RandomChoice->setOptions().");
+        }
+    }
+
     /**
     * Make a choice.
     *
